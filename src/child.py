207f607424a562bc823a6d8fd6ab3b09a8c9ac59
reno- <contact@rubiano.fr>
2016-01-14 13:18:36 +0100

    add power panel

--- src/child.py
+++ src/child.py
@@ -45,6 +45,34 @@ class Camera(QGroupBox,QModelIndex):
         # Create a camera
         self.camera = Visca(serial)
 
+        self.createPowerPanel()
+
+        # Create the main layout
+        mainLayout = QGridLayout()
+        # Integrate the layout previously created
+        mainLayout.addWidget(self.power_Groupbox)
+        self.mainLayout = mainLayout
+        # Integrate main layout to the main window
+        self.setLayout(mainLayout)
+
+    def power_pow_func(self):
+        pass
+
+    def createPowerPanel(self):
+        power_pow_label = QLabel('power')
+        power_pow = QPushButton()
+        power_pow.setCheckable(True)
+        power_pow.toggled.connect(self.power_pow_func)
+        self.power_pow = power_pow
+
+        power_layout = QHBoxLayout()
+        power_layout.addWidget(power_pow_label)
+        power_layout.addWidget(power_pow)
+        power_layout.addStretch(1)
+
+        self.power_Groupbox = QGroupBox()
+        self.power_Groupbox.setLayout(power_layout)   
+
     def newFile(self):
         """create a new camera"""
         self.isUntitled = True
