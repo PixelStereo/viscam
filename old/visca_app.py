ebfbe375c9c51005ece429f309ae2f97a58883c5
reno- <contact@rubiano.fr>
2016-03-18 16:32:50 +0100

    nice work… some works to do again

--- old/visca_app.py
+++ old/visca_app.py
@@ -1,11 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from time import sleep
-import threading
-
 import os, sys
-# for development of pyCamera, use git version
+# for development of viscam, use git version
 _path = os.path.abspath('./../libs')
 sys.path.append(_path)
 
@@ -19,44 +16,33 @@ serial = Serial()
 # open a connection on the serial object
 serial.open(portname='/dev/tty.usbserial-FTFNNBFM')
 v = Visca(serial)
-if debug : print '-----pyvisca module initialisation-----'
+
+if debug:
+	print '-----pyvisca module initialisation-----'
 cams = _cmd_adress_set(serial)
-if debug : print 'is there a camera somewhere? aka address_set :',cams
+if debug:
+	print 'is there a camera somewhere? aka address_set :',cams
 clear = _if_clear(serial)
-if debug : print 'clear all the camera buffers aka _if_clear :',clear
-if debug : print 'Turn off digital zoom aka zoom_digital(False)'
-if debug : print v.zoom_digital(False).encode('hex')
-if debug : print 'trig from APP :','datascreen off'
-if debug : print v.noOSD().encode('hex')
+if debug:
+	print 'clear all the camera buffers aka _if_clear :',clear
+if debug:
+	print 'Turn off digital zoom aka zoom_digital(False)'
+if debug:
+	print v.zoom_digital(False).encode('hex')
+if debug:
+	print 'trig from APP :','datascreen off'
+if debug:
+	print v.noOSD().encode('hex')
 
-# create OSC server
+# create OSC server for binding to v (instance of VISCA)
 osc = OSCServer(v, 22222, name='span')
-osc = osc.serverThread.oscServer
-# it will be nice to do next lines in devicemanager
-# create multi-thread server
-#st = threading.Thread(target=osc.serve_forever)
-#st.daemon = True
-#st.start()
-#if debug :print  "Server loop running in thread:", st.name
-if debug :print  '----------- VISCA APP LOADED AND RUNNING----------------'
 
-print '---------registering osc callback-----------------'
-# get properties of VISCA
-parameters = [p for p in dir(Visca) if isinstance(getattr(Visca, p),property)]
-# format to OSC (replace each underscore with a slash and add a slash as first character)
-for parameter in parameters:
-	old = parameter.split('_')
-	new = ''
-	for item in old:
-		new = new + '/' + item
-		handler = parameter + '_handler'
-	# some commands doesn't need arguments, but it's more simple to send all and ignore these after
-	function = 'def ' + handler + '(addr, tags, args, source):v.' + parameter + '(args)'
-	exec(function)
-	osc.addMsgHandler(parameter,eval(handler))
-	if debug:
-		dbg = 'register {parameter} -> {new}'
-		print(dbg.format(new=new, parameter=parameter))
-print '---------end of registering osc callback----------'
 
-#sleep(30)
+if debug:
+	print('----------- VISCA APP LOADED AND RUNNING----------------')
+
+
+if __name__ == '__main__':
+	print qq.q
+	while not qq.q.empty():
+		print qq.q.get()
