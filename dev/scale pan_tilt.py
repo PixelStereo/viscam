350a2c2206495806456ba595cc415894e0860f3d
reno- <contact@rubiano.fr>
2016-03-21 01:03:59 +0100

    better to comment

--- dev/scale pan_tilt.py
+++ dev/scale pan_tilt.py
@@ -1,14 +1,14 @@
 
 def translate(reply):
 	reply = reply.encode('hex')
-	def hex_unpack(zoom,L,size=2):
+	def hex_unpack(zoom, L, size=2):
 		part = zoom[:size]
-		zoom=zoom[size:]
+		zoom = zoom[size:]
 		L.append(part)
 		if zoom:
-			hex_unpack(zoom,L)
+			hex_unpack(zoom, L)
 			return L
-	reply = hex_unpack(reply,[])
+	reply = hex_unpack(reply, [])
 	a = reply[0]
 	b = reply[1]
 	c = reply[2]
@@ -21,19 +21,19 @@ def translate(reply):
 	return reply
 
 reply = '\x0E'+'\x01'+'\x0E'+'\x05'
-print 'E1E5',translate(reply)
+print 'E1E5 ->', translate(reply)
 reply = '\x01'+'\x0E'+'\x01'+'\x0B'
-print '1E1B',translate(reply)
+print '1E1B ->', translate(reply)
 
 reply = '\x0F'+'\x0C'+'\x07'+'\x05'
-print 'FC75',translate(reply)
+print 'FC75 ->', translate(reply)
 reply = '\x00'+'\x0F'+'\x0F'+'\x00'
-print '0FF0',translate(reply)
+print '0FF0 ->', translate(reply)
 
 reply = '\x0F'+'\x00'+'\x01'+'\x00'
-print 'F010',translate(reply)
+print 'F010 ->', translate(reply)
 reply = '\x00'+'\x03'+'\x08'+'\x0B'
-print '038B',translate(reply)
+print '038B ->', translate(reply)
 
 OldValue = 0
 OldMin = -170
@@ -49,6 +49,9 @@ print NewValue
 This is the result code
 """
 reply = 127
+# convert degree to visca pan
 NewValue = (((reply - -170) * (24094 - 45537)) / (170 - -170)) + 45537
+# convert visca pan to degree
 NewValue = (((NewValue - 45537) * (170 - -170)) / (24094 - 45537)) + -170
+
 print NewValue
