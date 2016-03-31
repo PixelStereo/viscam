00c8380c9acf8bc14a0781afb356eb8e67db0635
reno- <contact@rubiano.fr>
2016-03-31 17:47:58 +0200

    don't use threading

--- src/visca_app_test.py
+++ src/visca_app_test.py
@@ -12,13 +12,12 @@ v = Viscam('/dev/tty.usbserial-FTFNNBFM').get_instances()[0]
 libs_path = os.path.abspath('./../3rdparty/pydevicemanager')
 sys.path.append(libs_path)
 from pydevicemanager.osc import OSCServer
-osc = OSCServer(v, 22222, name='viscam', threading=True)
+osc = OSCServer(v, 22222, name='viscam', threading=False)
 
 if __name__ == "__main__" :
 	# loop and dispatch messages every 100ms
 	try:
 		while 1:
-			pass
-			#osc.server.recv(10)
+			osc.server.recv(10)
 	except KeyboardInterrupt:
 		quit()
