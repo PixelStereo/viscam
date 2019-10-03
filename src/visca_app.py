5a35af4f930c1a65d5f000b482ae1a92b8762bff
Renaud Rubiano <reno@blacky-1.home>
2019-10-03 14:23:20 +0200

    clean up a bit

--- src/visca_app.py
+++ src/visca_app.py
@@ -4,24 +4,20 @@
 import os, sys
 
 from pyviscam.broadcast import v_cams
+from pydevicemanager.osc import OSCServer
 
 # a camera is created in visca_app
 # create a visca bus object
 cams = v_cams()
 # get a list of serial ports available and select the last one
 ports = cams.serial.listports()
-
 port = ports[0]
-
 # open a connection on the serial object
 cams.reset(port)
 cams = cams.get_instances()
 v = cams[0]
 
 # create OSC server for binding to v (instance of VISCA)
-libs_path = os.path.abspath('./../3rdparty')
-sys.path.append(libs_path)
-from pydevicemanager.osc import OSCServer
 osc = OSCServer(v, 22222, name='viscam')
 
 if __name__ == "__main__" :
